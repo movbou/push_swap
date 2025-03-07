3267c4e3a35acf40929aeeb81ab6b759da18dd6b
movbou <movbo43@gmail.com>
2025-03-07 09:53:27 +0000

    added libft lst

---
+++ create_stacks.c
@@ -0,0 +1,33 @@
+#include "push_swap.h"
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	temp = *lst;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+}
+t_list *create_the_stack(int n_element)
+{
+
+}
--- push_swap.h
+++ push_swap.h
@@ -8,7 +8,7 @@
 
 typedef struct s_list
 {
-	int				*data;
+	int				*content;
 	struct s_list	*next;
 }					t_list;
 
