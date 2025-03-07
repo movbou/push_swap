3109c147929334f6f2b10e02b468623bcb02f651
Anas Chajar <achajar@c4r7p3.1337.ma>
2025-03-07 16:47:52 +0000

    swiching to addfront

--- create_stacks.c
+++ create_stacks.c
@@ -11,40 +11,16 @@ t_list	*ft_lstnew(int content)
 	node->next = NULL;
 	return (node);
 }
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	push(t_list **lst, int content)
 {
-	t_list	*temp;
-
-	if (!lst || !new)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
+	if (!lst || !content) {
+		return;
 	}
-	temp = *lst;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
-t_list	*create_the_stack(int n_element)
-{
-	t_list	*head;
 	t_list	*node;
-	t_list *temp;
 
-head = ft_lstnew(0);
-	node = NULL;
-	
-	while (n_element)
-	{
-		
-		node = ft_lstnew(0);
-		ft_lstadd_back(&head, node);
-		node->next = NULL;
-	}
-
-	return (head);
+	node = ft_lstnew(content);
+	node->content= content;
+	node->next = *lst;
+	*lst = node;
 }
 
