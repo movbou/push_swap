929514405b97f1d563ec84ab4340e3484991406b
Anas Chajar <achajar@c3r4p2.1337.ma>
2025-03-10 20:33:04 +0000

    added some utils

--- utils.c
+++ utils.c
@@ -1,4 +1,19 @@
 #include "push_swap.h"
+#include <stdlib.h>
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+
+	if (!lst || !del || !*lst)
+		return ;
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		free(lst);
+		*lst = temp;
+	}
+}
 
 t_list	*ft_lstnew(int content)
 {
@@ -11,29 +26,54 @@ t_list	*ft_lstnew(int content)
 	node->next = NULL;
 	return (node);
 }
+
 void	push(t_list **lst, int content)
 {
-	if (!lst || !content) {
-		return;
-	}
 	t_list	*node;
 
+	if (!lst || !content)
+	{
+		return ;
+	}
 	node = ft_lstnew(content);
-	node->content= content;
+	node->content = content;
 	node->next = *lst;
 	*lst = node;
 }
 
-void swap_first_2elements(t_list *stack)
+void	swap_first_2elements(t_list *stack)
 {
+	t_list	*temp;
 
+	if (!stack)
+	{
+		return ;
+	}
+	temp = stack->next;
+	stack->next = stack->next->next;
+	stack->next->next = temp;
 }
-void swap_heads(t_list stack_a,t_list stack_b)
-{
 
+void	swap_heads(t_list *stack_a, t_list *stack_b)
+{
+	t_list	*temp;
+
+	if (!stack_a || !stack_b)
+		return ;
+	temp = stack_a->next;
+	stack_a->next = stack_b->next;
+	stack_b->next = temp;
 }
 
-void rotate(t_list *stack)
+t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
 
+void	rotate(t_list *stack)
+{
 }
