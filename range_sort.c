aa4f9f93a7912b64a3eeeb55cad8537120fddb72
Anas Chajar <achajar@e2r2p3.1337.ma>
2025-03-23 06:10:33 +0000

    added the core algorithm

--- range_sort.c
+++ range_sort.c
@@ -1,5 +1,51 @@
 #include "push_swap.h"
 
+int	max_index(t_list *stack)
+{
+	int	i;
+	int	max_index_holder;
+	int	holder_of_max;
+
+	i = 0;
+	holder_of_max = stack->content;
+	while (stack)
+	{
+		if (holder_of_max < stack->content)
+		{
+			holder_of_max = stack->content;
+			max_index_holder = i;
+		}
+		stack = stack->next;
+		i++;
+	}
+	return (max_index_holder);
+}
+
+void	sort_back_to_a(t_list *stack_a, t_list *stack_b)
+{
+	int	half_size_b;
+	int	max_index;
+	int	i;
+
+	half_size_b = ft_lstsize(stack_b) / 2;
+	while (stack_a)
+	{
+		max_index = max_index(stack_b);
+		if (max_index < half_size_b)
+		{
+			i = max_index;
+			while (i--)
+				rotate_b(&stack_b);
+		}
+		if (max_index >= half_size_b)
+		{
+			i = max_index - ft_lstsize(stack_b);
+			while (i--)
+				rrotate_b(&stack_b);
+		}
+	}
+}
+
 void	move_to_b(int *start, int *end, t_list *stack_a, t_list *stack_b)
 {
 	int	i;
@@ -32,7 +78,8 @@ void	move_to_b(int *start, int *end, t_list *stack_a, t_list *stack_b)
 		}
 	}
 }
-void	sort_stack(t_list *stack_a, t_list *stack_b)
+
+void push_bak_to_a void sort_stack(t_list *stack_a, t_list *stack_b)
 {
 	int	list_length;
 	int	end;
